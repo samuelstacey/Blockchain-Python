242a9a418c077ee81aee7ecf5f8a0e50bc616438
Samuel Stacey <s.stacey@live.co.uk>
2020-02-09 21:13:47 +0000

    Bugs on balance fixed

--- blockchain.py
+++ blockchain.py
@@ -105,11 +105,11 @@ def get_balance(participant):
                       for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
     #UNDERSTAND THIS
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > else tx_sum + 0, tx_sender, 0)
     tx_receiver = [[tx['amount'] for tx in block['transactions']
                     if tx['recipient'] == participant] for block in blockchain]
     #UNDERSTAND THIS
-    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_receiver, 0)
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_receiver, 0)
     return amount_received - amount_sent
 
 
